287ae93b6dfc8388131a236f66d0a9f1439d1807
igorraknorwid <igorrakfed@gmail.com>
2024-03-01 14:52:10 +0100

    5minutes

--- src/components/visit_maker/VisitMaker.tsx
+++ src/components/visit_maker/VisitMaker.tsx
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { useState, useEffect } from "react";
 import { observer } from "mobx-react-lite";
 
 interface IVisitMaker {
@@ -20,7 +20,7 @@ interface Minute {
 }
 
 function createMinutesArray(
-  totalMinutes: number,
+  totalUnits: number,
   start: number,
   finish: number,
   hiden_time_start: number,
@@ -29,41 +29,44 @@ function createMinutesArray(
   const minutesArray: Minute[] = [];
 
   const setIsOpen = (i: number) => {
-    if (i > start - 1 && i < finish + 1) {
-      return true;
-    } else {
-      return false;
-    }
+    const minute = i * 5;
+    return minute > start - 1 && minute < finish + 1;
   };
 
   const setIsHide = (i: number) => {
-    if (i > hiden_time_start && i < hiden_time_end) {
-      return true;
-    } else {
-      return false;
-    }
+    const minute = i * 5;
+    return minute > hiden_time_start && minute < hiden_time_end;
   };
 
   const setHour = (i: number) => {
-    const time = i / 60;
-    if (Number.isInteger(time) && i % 60 === 0 && time !== 0) {
-      return time;
-    } else {
-      return null;
-    }
+    const minute = i * 5;
+    const time = minute / 60;
+    return Number.isInteger(time) && minute % 60 === 0 && time !== 0
+      ? time
+      : null;
   };
 
-  for (let i = 0; i < totalMinutes; i++) {
+  for (let i = 0; i < totalUnits; i++) {
     minutesArray.push({
       number: i,
       isFree: true,
       isOpenTime: setIsOpen(i),
       isHiden: setIsHide(i),
       hour: setHour(i),
+      isReserved: null,
     });
   }
 
   return minutesArray;
+}
+
+function minuteToTime(unit: number): string {
+  const minutes = unit * 5;
+  const hours = Math.floor(minutes / 60);
+  const mins = minutes % 60;
+  const formattedHours = hours < 10 ? `0${hours}` : `${hours}`;
+  const formattedMins = mins < 10 ? `0${mins}` : `${mins}`;
+  return `${formattedHours}.${formattedMins}`;
 }
 
 function VisitMaker({
@@ -72,11 +75,13 @@ function VisitMaker({
   hiden_time_start,
   hiden_time_end,
 }: IVisitMaker) {
-  const [day, setDay] = React.useState([] as Minute[]);
-  React.useEffect(() => {
-    const totalMinutes = 1440; // Total minutes in a day
+  const [day, setDay] = useState([] as Minute[]);
+  const [time, setTime] = useState<string | null>(null);
+
+  useEffect(() => {
+    const totalUnits = 1440 / 5; // Total 5-minute units in a day
     const minutesOfDay: Minute[] = createMinutesArray(
-      totalMinutes,
+      totalUnits,
       start,
       finish,
       hiden_time_start,
@@ -84,8 +89,10 @@ function VisitMaker({
     );
     setDay(minutesOfDay);
   }, []);
+
   return (
     <div className='my-8'>
+      <div className='my-10'>{time}</div>
       <div className='flex'>
         <div className='flex flex-col w-10'>
           {day &&
@@ -93,11 +100,12 @@ function VisitMaker({
               (minute) =>
                 !minute.isHiden && (
                   <div
-                    className={`relative h-[1px] w-ful ${
-                      minute.hour && "bg-gray-500"
+                    key={minute.number}
+                    className={`relative h-[5px] w-full ${
+                      minute.hour !== null && "bg-gray-500"
                     }`}
                   >
-                    {minute.hour && (
+                    {minute.hour !== null && (
                       <div className='absolute w-full left-0 bottom-0'>
                         {minute.hour}
                       </div>
@@ -112,19 +120,27 @@ function VisitMaker({
               (minute) =>
                 !minute.isHiden && (
                   <div
-                    className={`h-[1px] w-ful cursor-pointer ${
+                    key={minute.number}
+                    className={`h-[5px] w-full cursor-pointer ${
                       minute.isOpenTime
-                        ? `hover:bg-green-500 ${
-                            minute.number % 60 === 0
-                              ? `bg-black`
+                        ? `${!minute.isReserved && "hover:bg-green-700"} ${
+                            minute.number % 12 === 0
+                              ? "bg-black"
                               : `${
-                                  (minute.number % 60) % 15 === 0
-                                    ? `bg-red-500/50`
-                                    : `bg-blue-500/50`
+                                  minute.number % 3 === 0
+                                    ? "bg-gray-500/50"
+                                    : "bg-blue-500/50"
                                 }`
                           }`
-                        : `bg-grey-500`
+                        : "bg-grey-500"
                     }`}
+                    onMouseOver={() => {
+                      const time = minuteToTime(minute.number);
+                      setTime(time);
+                    }}
+                    onMouseLeave={() => {
+                      setTime(null);
+                    }}
                   ></div>
                 )
             )}
